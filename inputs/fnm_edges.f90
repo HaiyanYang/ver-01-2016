5812f5c17e2f6768090dd56d06f352d382917e61
BoyangChen <qinyuhua@Yuhuas-MacBook-Air.local>
2015-12-20 11:21:26 +0800

    test inputs

--- inputs/fnm_edges.f90
+++ inputs/fnm_edges.f90
@@ -4,19 +4,9 @@ use edge_list_module, only: edge_list
                                       
   integer :: nedge=0                  
                                       
-  nedge=16              
+  nedge=96              
   allocate(edge_list(nedge))          
-  call update(edge_list(1), constrained=.true.) 
-  call update(edge_list(5), constrained=.true.) 
-  call update(edge_list(9), constrained=.true.) 
-  call update(edge_list(13), constrained=.true.) 
-  call update(edge_list(3), constrained=.true.) 
-  call update(edge_list(7), constrained=.true.) 
-  call update(edge_list(11), constrained=.true.) 
-  call update(edge_list(15), constrained=.true.) 
-  call update(edge_list(13), constrained=.true.) 
-  call update(edge_list(16), constrained=.true.) 
-  call update(edge_list(14), constrained=.true.) 
-  call update(edge_list(15), constrained=.true.) 
+  call update(edge_list(72), constrained=.true.) 
+  call update(edge_list(4), constrained=.true.) 
 
 end subroutine set_fnm_edges
--- inputs/fnm_predelam.f90
+++ inputs/fnm_predelam.f90
@@ -3,5 +3,13 @@ use predelam_list_module, only: predelam_elems, predelam_interf
                                       
   integer :: npdelem                  
                                       
+  npdelem=1              
+
+  allocate(predelam_elems(npdelem))     
+  allocate(predelam_interf)             
+
+  predelam_elems(1)=1
+
+  predelam_interf=1
 
 end subroutine set_fnm_predelam
